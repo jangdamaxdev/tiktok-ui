0478e2f46d7eec3bcdf8051785945d37d4be654b
Jangdamaxdev <jangdamax.dev@gmail.com>
2024-11-13 12:37:58 +0700

    95. Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,13 @@
-function Sidebar() {
-    return ( <h2>Sidebar</h2> );
+import classNames from 'classnames/bind'
+import styles from './Sidebar.module.scss'
+
+const cx = classNames.bind(styles)
+function Sidebar({ children }) {
+  return (
+    <aside className={cx('wrapper')}>
+      <div className={cx('content')}><p>1</p>{children}</div>
+    </aside>
+  )
 }
 
-export default Sidebar;
+export default Sidebar
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,18 @@
+import classNames from 'classnames/bind'
+import styles from './DefaultLayout.module.scss'
 import Header from './Header'
 import Sidebar from './Sidebar'
 
 function DefaultLayout({ children }) {
+  const cx = classNames.bind(styles)
+  console.log(styles);
+  
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx("content")}>{children}</div>
       </div>
     </div>
   )
